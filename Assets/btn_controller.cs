48f2819ea665d54a8fce43d3beefe3b19431dd83
Pasin Charoensuk <pasin.c@rmutsvmail.com>
2022-05-20 20:38:35 +0700

    Upload All

---
+++ Assets/btn_controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btn_controller : MonoBehaviour
+{
+    public string url = "https://drive.google.com/file/d/1kgzDLgxFYufmRmckmGrz13PNaoUIsLWu/view?usp=sharing";
+    public void open()
+    {
+        Application.OpenURL(url);
+    }
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void PlayScene()
+    {
+        SceneManager.LoadScene("Play");
+    }
+    public void AuthorScene()
+    {
+        SceneManager.LoadScene("Authors");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/play.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class play : MonoBehaviour
+{
+    public void AR_Bull()
+    {
+        SceneManager.LoadScene("AR_Bull");
+    }
+    public void AR_Chicken()
+    {
+        SceneManager.LoadScene("AR_Chicken");
+    }
+    public void AR_Crane()
+    {
+        SceneManager.LoadScene("AR_Crane");
+    }
+    public void AR_Goat()
+    {
+        SceneManager.LoadScene("AR_Goat");
+    }
+    public void AR_Rabbit()
+    {
+        SceneManager.LoadScene("AR_Rabbit");
+    }
+    public void AR_Pig()
+    {
+        SceneManager.LoadScene("AR_Pig");
+    }
+
+    public void how_pig()
+    {
+        SceneManager.LoadScene("how_pig");
+    }
+    public void how_rabbit()
+    {
+        SceneManager.LoadScene("how_rabbit");
+    }
+    public void how_goat()
+    {
+        SceneManager.LoadScene("how_goat");
+    }
+    public void how_chicken()
+    {
+        SceneManager.LoadScene("how_chicken");
+    }
+    public void how_crane()
+    {
+        SceneManager.LoadScene("how_crane");
+    }
+    public void how_bull()
+    {
+        SceneManager.LoadScene("how_bull");
+    }
+}
